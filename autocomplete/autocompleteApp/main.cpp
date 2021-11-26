6e5ced98954f15a46ba8841898e301b0d5d5fa8d
EddieSpaghetti666 <Dsmith141996@gmail.com>
2021-11-25 21:35:46 -0500

    Added application to demonstate autocompletion

---
+++ autocomplete/autocompleteApp/main.cpp
@@ -0,0 +1,33 @@
+#include <Autocompleter.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+std::vector<std::string> loadDictionary(std::string fileName) {
+  std::fstream fs(fileName);
+  std::vector<std::string> dictionary;
+  std::string word;
+  while (fs >> word) {
+    std::transform(word.begin(), word.end(), word.begin(), std::tolower);
+    dictionary.emplace_back(word);
+  }
+  return dictionary;
+}
+int main() {
+  const auto dictionary = loadDictionary("fruits.txt");
+  Autocompleter ac(dictionary);
+  while (true) {
+    std::string prefix;
+    std::cout << "Please enter a prefix you want autocompleted:\n"
+              << std::endl;
+    std::cin >> prefix;
+    const auto words = ac.getAllStringsFor(prefix);
+    for (const auto& word : words) {
+      std::cout << word << std::endl;
+    }
+  }
+  return 0;
+}
